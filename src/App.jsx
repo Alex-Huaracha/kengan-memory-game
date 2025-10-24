eaf2841250d0bc85a19f56d95279cec2b8406da6
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-10-24 14:50:58 -0500

    feat: implement character fetching logic and loading/error states in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,53 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
+// Kengan Ashura ID : 40269
+const ANIME_ID = 40269;
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchKenganCharacters = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `https://api.jikan.moe/v4/anime/${ANIME_ID}/characters`
+        );
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch Kengan Ashura characters');
+        }
+
+        const data = await response.json();
+        // console.log(data);
+
+        const formattedCards = data.data.map((item) => ({
+          id: item.character.mal_id,
+          name: item.character.name,
+          image: item.character.images.webp.image_url,
+        }));
+
+        console.log(formattedCards);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchKenganCharacters();
+  }, []);
+
+  if (loading) {
+    return <div className="loading">Loading Characters...</div>;
+  }
+
+  if (error) {
+    return <div className="error">Error: {error}</div>;
+  }
+
   return (
     <>
       {/* Header */}
