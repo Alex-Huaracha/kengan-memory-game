3035061545ccef34a3a0c77ac1f4cd39a10e40cf
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-10-24 09:32:34 -0500

    feat: implement header layout with logo and title, and add font styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      {/* Header */}
+      <header>
+        <img className="logo" src="/src/assets/images/logo.png" alt="Logo" />
+        <h1 className="title">Memory Card</h1>
+      </header>
+      {/* Main Content */}
+
+      {/* Footer */}
+      {/* <footer>
+        <p>© 2024 My Company</p>
+      </footer> */}
+    </>
+  );
 }
 
 export default App;
